74b4a0a108ca2d9358bda895baaad46f71e983f7
Matthew Kudija <m.kudija@gmail.com>
2022-02-09 20:32:04 -0500

    fixes

--- daily-bible-obsidian.py
+++ daily-bible-obsidian.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-# Python Version: 3.8.x
-
 import pandas as pd
 import numpy as np
 import os
@@ -10,11 +7,12 @@ import requests
 
 import datetime as datetime
 currentDate = datetime.datetime.today().date()
+# currentDate = datetime.datetime.today().date() - datetime.timedelta(days = 2)
 
 
 def scrape_readings(currentDate):
     page = requests.get('http://www.usccb.org/bible/readings/{}.cfm'.format(currentDate.strftime('%m%d%y')))
-    soup = BeautifulSoup(page.content, "html.parser")
+    soup = BeautifulSoup(page.content, 'html.parser')
     h2 = soup.find_all('h2')
     h3 = soup.find_all('h3')
     add = soup.find_all(lambda tag: tag.name == 'div' and tag.get('class') == ['address'])
@@ -51,7 +49,19 @@ def print_readings_obsidian(currentDate, h2, h3, add):
             verse = str(add[i].contents[1].contents[0]).replace('\n','')
         except: # I think this is an issue when there are options for the Gospel? (i.e. on Feast of the Nativity of the Blessed Virgin Mary 2021-09-08)
             verse = ''
-        verseFront = verse.split(':')[0].replace(' ','-')
+        # print(verse)
+
+        # for numbered books, defined here: https://github.com/mkudija/BibleGateway-to-Obsidian-Catholic/blob/master/bg2obs.sh#L70    
+        num_books = ['1 Sm ', '2 Sm ', '1 Kgs ', '2 Kgs ', '1 Chr ', '2 Chr ', '1 Mc ', '2 Mc ', '1 Cor ', '2 Cor ', 
+                    '1 Thes ', '2 Thes ', '1 Tm ', '2 Tm ', '1 Pt ', '2 Pt ', '1 Jn ', '2 Jn ', '3 Jn ']
+        if [x for x in num_books if(x in verse)]:
+            verseFront = verse.split(':')[0]
+            verseFront = verseFront.split(' ')[0]+' '+verseFront.split(' ')[1]+'-'+verseFront.split(' ')[2] # replace second space with '-'
+            if int(verseFront.split('-')[1]) < 10:
+                verseFront = verseFront.split('-')[0] + '-0' + verseFront.split('-')[1]
+        else:
+            verseFront = verse.split(':')[0].replace(' ','-')
+
         try:
             if int(verseFront.split('-')[1]) < 10:
                 verseFront = verseFront.split('-')[0] + '-0' + verseFront.split('-')[1]
@@ -61,6 +71,7 @@ def print_readings_obsidian(currentDate, h2, h3, add):
         verseLink = '[[{}]]:{}'.format(verseFront, verseBack)
         if reading != 'Alleluia' and len(reading)>0:
             print('{}: {}'.format(reading, verseLink))
+
 
 def main():
 
